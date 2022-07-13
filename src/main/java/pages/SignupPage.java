0ee96c19d45b3c222de050f07fdb7de3501a1f6c
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-07-13 17:43:14 +0600

    Update SignupPage.java

--- src/main/java/pages/SignupPage.java
+++ src/main/java/pages/SignupPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import drivers.PageDriver;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -28,7 +29,7 @@ public class SignupPage {
     @FindBy(xpath = "//button[normalize-space()='SIGN UP']")
     WebElement signupBtn;
 
-
+    @Step("Enter signup user info")
     public void signupInfo(String name, String email, String password) {
         sendText(nameInput, name);
         sendText(emailInput, email);
@@ -36,6 +37,7 @@ public class SignupPage {
         agree.click();
     }
 
+    @Step("Click on signup submit button")
     public void signupSubmit() {
         signupBtn.click();
     }

11aacd968f73a8aed2c6477760b706c68ae2cdb0
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-07-13 17:43:07 +0600

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import drivers.PageDriver;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -20,18 +21,29 @@ public class HomePage {
     WebElement loginButton;
 
     @FindBy(xpath = "//a[normalize-space()='Sign out']")
-    List<WebElement> signoutButton;
+    WebElement signoutButton;
 
+    @FindBy(xpath = "//a[normalize-space()='Sign out']")
+    List<WebElement> signoutButtonList;
+
+    @Step("Click on signup button")
     public void signupButtonClick() {
         signupButton.click();
     }
 
+    @Step("Click on login button")
     public void loginButtonClick() {
         loginButton.click();
     }
 
+    @Step("Return signout button count")
     public int signoutButtonSize() {
-        return signoutButton.size();
+        return signoutButtonList.size();
+    }
+
+    @Step("Click on signout button")
+    public void logout() {
+        signoutButton.click();
     }
 
 }
